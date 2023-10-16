bf99aa6656ee3561b7446b90bb7c794c7ffafaa8
RAHULCYBERGEEK <140412922+RAHULCYBERGEEK@users.noreply.github.com>
2023-10-16 19:34:19 +0530

    Add files via upload

---
+++ ternary_operator.java
@@ -0,0 +1,10 @@
+class ternary_operator
+{
+   public static void main(String ...args)
+   {
+     int a=20,b=40,max;
+     max = (a<b) ? a : b;
+     System.out.println("max = "+ max);
+   }
+}
+   
